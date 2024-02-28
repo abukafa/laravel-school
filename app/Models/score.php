82a2e66b3676b1943bd2dc80001a3f393bd92f96
jaz-animation <jaz.animation@hotmail.com>
2024-02-28 12:32:38 +0700

    fix namespace

--- app/Models/score.php
+++ app/Models/score.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class score extends Model
+class Score extends Model
 {
     use HasFactory;
     protected $guarded = [];
